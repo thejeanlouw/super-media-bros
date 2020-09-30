19072c10b32d54fb29fb36cb2f030fbbc2719d22
Jean Louw <jlouw@iqbusiness.net>
2020-09-30 15:37:58 +0200

    Updates

---
+++ precache-manifest.22753d140816ed62625dc7ba27cce4f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d591ea9a297f75ddbd2b25c04980bafc",
+    "url": "/super-media-bros/index.html"
+  },
+  {
+    "revision": "eb7cb5297056ed8e520f",
+    "url": "/super-media-bros/static/css/main.11c9367f.chunk.css"
+  },
+  {
+    "revision": "70946edd0fa361a7d2fb",
+    "url": "/super-media-bros/static/js/2.4f47c423.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/super-media-bros/static/js/2.4f47c423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb7cb5297056ed8e520f",
+    "url": "/super-media-bros/static/js/main.61eb885c.chunk.js"
+  },
+  {
+    "revision": "eebbd81899b606fd618e",
+    "url": "/super-media-bros/static/js/runtime-main.0e2a11fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/super-media-bros/static/media/logo.5d5d9eef.svg"
+  }
+]);
